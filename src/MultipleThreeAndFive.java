c0453e8476905c27862d4328c5d161f3c9b6c63b
Fred <gundam0083ster@gmail.com>
2014-08-13 09:35:38 +0800

    first commit

--- src/MultipleThreeAndFive.java
+++ src/MultipleThreeAndFive.java
@@ -6,7 +6,7 @@ public class MultipleThreeAndFive {
      */
     public static void main(String[] args) {
 	// TODO Auto-generated method stub
-	System.out.println("The sum of all the multiples of 3 or 5 below 1000 is "+sumOfMultipleThreeAndFive(10));
+	System.out.println("The sum of all the multiples of 3 or 5 below 1000 is "+sumOfMultipleThreeAndFive(1000));
     }
     
     static int sumOfMultipleThreeAndFive(int range)
